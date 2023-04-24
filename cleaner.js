3222d3e7956846ef18ff5eab577c3b6ab15db676
Akif Yüce <70021050+Akif9748@users.noreply.github.com>
2023-04-24 22:20:59 +0300

    Better code, and added optinal support for messageID

--- cleaner.js
+++ cleaner.js
@@ -5,162 +5,132 @@ import readline from 'readline';
 
 // Read data from data.txt
 const data = fs.readFileSync('data.txt', 'utf-8').split('\n');
-const youruser = data[0].trim(); // Discord username
-const authToken = data[1].trim(); // Discord token
-const channel = data[2].trim(); // Channel ID
-let lastmsg = data[3].trim(); // Latest message ID
+const authToken = data[0].trim(); // Discord token
+const channel = data[1].trim(); // Channel ID
+let lastmsg = data[2].trim(); // Optional last Message ID
 let firstIteration = true; // A flag to determine if the current iteration is the first run of the clearMessages function
 
-const limit = parseInt(data[4]) || 0; // Optional limit for messages to delete
-const delay = 10 * 1000; // Constant delay in milliseconds (10 seconds)
+const limit = parseInt(data[3]) || 0; // Optional limit for messages to delete
+const delay = 2000; // Constant delay in milliseconds (2 seconds is enough)
 
-// Counter and increment function for deleted messages
-let deletedMessagesCount = 0;
-function incrementCounter() {
-  deletedMessagesCount++;
-  return deletedMessagesCount;
-}
+const baseURL = `https://discord.com/api/v10/channels/${channel}/messages`;
+const headers = { "Authorization": authToken };
 
 // Log messages with a timestamp using the current system time
-function logMessage(message) {
-  const timestamp = new Date().toLocaleString();
-  const log = `[${timestamp}] ${message}\n`;
-  fs.appendFileSync('log.txt', log, 'utf-8');
-  console.log(log.trim());
+const logMessage = message => {
+    const log = `[${new Date().toLocaleString()}] ${message}\n`;
+    fs.appendFileSync('log.txt', log, 'utf-8');
+    console.log(log.trim());
 }
 
 // Prompt the user for confirmation before proceeding with message deletion
-async function promptUserConfirmation() {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+const promptUserConfirmation = () => new Promise(resolve => readline.createInterface(process.stdin, process.stdout).question('Are you sure you want to start deleting your messages? This action cannot be undone! Type "yes" if you wish to continue or you can enter any other key to cancel: ', answer => {
+    rl.close();
+    resolve(answer.toLowerCase().includes("y"));
+}));
 
-  return new Promise((resolve) => {
-    rl.question('Are you sure you want to start deleting your messages? This action cannot be undone! Type "yes" if you wish to continue or you can enter any other key to cancel: ', (answer) => {
-      rl.close();
-      resolve(answer.toLowerCase() === 'yes');
-    });
-  });
+// Validate the provided username and token by fetching the current user's information from the Discord API
+const fetchUser = async () => {
+    const userResponse = await fetch('https://discord.com/api/v10/users/@me', { headers });
+
+    if (userResponse.status === 401)
+        return logMessage("Error: Invalid token. Double check the token you entered and try again.");
+
+    return await userResponse.json();
 }
 
 // Store the ID of the last fetched message in the previous iteration, used to identify if there are no more messages to delete
-let lastFetchedMessage = "";
-
-// Validate the provided username and token by fetching the current user's information from the Discord API
-async function validateUsername() {
-  const headers = { "Authorization": authToken };
-  const userResponse = await fetch('https://discord.com/api/v10/users/@me', { headers, method: 'GET' });
-
-  if (userResponse.status === 401) {
-    logMessage("Error: Invalid token. Double check the token you entered and try again.");
-    return false;
-  }
-
-  const currentUser = await userResponse.json();
-  if (currentUser.username !== youruser) {
-    logMessage("Error: The entered username does not match the username associated with the provided token. Try again and make sure you entered your username correctly without the # and numbers after it.");
-    return false;
-  }
-
-  return true;
-}
+let lastFetchedMessage = "", errorCount = deletedMessagesCount = 0;
 
 // Main function to clear messages in a Discord channel
 async function clearMessages() {
-  if (limit > 0 && deletedMessagesCount >= limit) {
-    logMessage("Message limit reached, stopping execution.");
-    return;
-  }
+    if (limit > 0 && deletedMessagesCount >= limit)
+        return logMessage("Message limit reached, stopping execution.");
 
-  const baseURL = `https://discord.com/api/v10/channels/${channel}/messages`;
-  const headers = { "Authorization": authToken };
+    // Fetch and check user
+    const currentUser = await fetchUser();
+    if (!currentUser) return;
 
-  // Check for invalid token and username before proceeding
-  const isUsernameValid = await validateUsername();
-  if (!isUsernameValid) {
-    return;
-  }
+    try {
 
-  try {
-  const response = await fetch(baseURL + "?before=" + (BigInt(lastmsg) + BigInt(1)), { headers, method: 'GET' });
+        // If last message is not defined, it can find the last message from user.
+        if (!lastmsg)
+            lastmsg = await fetch(baseURL + "/search?author_id=" + currentUser.id + "&limit=1", { headers })
+                .then(m => m.json()).then(m => m.messages[0][0].id);
 
-  const messages = await response.json();
+        const response = await fetch(baseURL + "?before=" + (BigInt(lastmsg) + BigInt(1)), { headers });
 
-  // Check for wrong channel ID
-  if (response.status === 404 || messages.code === 10003) {
-    logMessage("Error: Unknown channel. Please double check the channel ID you entered and try again.");
-    return;
-  }
+        const messages = await response.json();
 
-  // Check if messages were fetched successfully
-  if (!Array.isArray(messages)) {
-    logMessage("Error: Failed to fetch messages from the Discord API. This may be due to network issues. Try again later.");
-    return;
-  }
+        // Check for wrong channel ID
+        if (response.status === 404 || messages.code === 10003)
+            return logMessage("Error: Unknown channel. Please double check the channel ID you entered and try again.");
 
-  if (messages.length === 0) {
-    logMessage("No more messages to delete, stopping execution.");
-    if (firstIteration) {
-      logMessage("If you are seeing this message right after you tried to run the program and you were not even prompted for confirmation, this means you entered an invalid message ID. Messages will only be deleted if you entered the correct message ID and chose to continue after you were prompted for confirmation.");
-    }
-    return;
-  }
+        // Check if messages were fetched successfully
+        if (!Array.isArray(messages))
+            return logMessage("Error: Failed to fetch messages from the Discord API. This may be due to network issues. Try again later.");
 
-    // Check if the last fetched message is the same as in the previous iteration
-    if (lastFetchedMessage === messages[messages.length - 1].id) {
-      logMessage("No more messages to delete, stopping execution.");
-      return;
-    }
-  
-    // Update the lastFetchedMessage variable
-    lastFetchedMessage = messages[messages.length - 1].id;
+        if (!messages.length)
+            return logMessage(firstIteration ?
+                "If you are seeing this message right after you tried to run the program and you were not even prompted for confirmation, this means you entered an invalid message ID. Messages will only be deleted if you entered the correct message ID and chose to continue after you were prompted for confirmation."
+                : "No more messages to delete, stopping execution."
+            );
 
-  if (firstIteration) {
-    const targetMessage = messages.find(message => message.id === lastmsg);
-    if (!targetMessage) {
-      logMessage("Error: The provided message ID does not exist in the specified channel. Please double check the message ID and try again.");
-      return;
-    }
 
-    if (targetMessage.author.username !== youruser) {
-      logMessage("Error: The provided message ID does not belong to the specified user; you can only delete messages you have sent. Please double check the message ID and try again.");
-      return;
-    }
+        // Check if the last fetched message is the same as in the previous iteration
+        const newLastFetchedMessage = messages.at(-1).id;
 
-        // Prompt the user to confirm whether they want to proceed with message deletion and handle their response
-        const userConfirmation = await promptUserConfirmation();
-        if (!userConfirmation) {
-          logMessage("User cancelled the operation. No messages will be deleted.");
-          return;
+        if (lastFetchedMessage === newLastFetchedMessage)
+            return logMessage("No more messages to delete, stopping execution.");
+
+        // Update the lastFetchedMessage variable
+        lastFetchedMessage = newLastFetchedMessage;
+
+        if (firstIteration) {
+            firstIteration = false; // Set the firstIteration flag to false
+
+            const targetMessage = messages.find(message => message.id === lastmsg);
+
+            if (!targetMessage)
+                return logMessage("Error: The provided message ID does not exist in the specified channel. Please double check the message ID and try again.");
+
+            // to-do: maybe it is not require to check
+            if (targetMessage.author.id !== currentUser.id)
+                return logMessage("Error: The provided message ID does not belong to the specified user; you can only delete messages you have sent. Please double check the message ID and try again.");
+
+            // Prompt the user to confirm whether they want to proceed with message deletion and handle their response
+            const userConfirmation = await promptUserConfirmation();
+            if (!userConfirmation)
+                return logMessage("User cancelled the operation. No messages will be deleted.");
+
         }
-  }
 
-  for (const message of messages) {
-    lastmsg = message.id;
+        for (const message of messages) {
+            lastmsg = message.id;
 
-    if (message.author.username === youruser) {
-      const count = incrementCounter();
+            if (message.author.id === currentUser.id) {
 
-      await new Promise(resolve => setTimeout(resolve, delay));
-      await fetch(`${baseURL}/${message.id}`, { headers, method: 'DELETE' });
+                await new Promise(resolve => setTimeout(resolve, delay));
+                const res = await fetch(`${baseURL}/${message.id}`, { headers, method: 'DELETE' });
 
-      logMessage(`Completed: ${count}`);
+                if (res.ok)
+                    logMessage(`Completed: ${++deletedMessagesCount}`);
+                else
+                    logMessage("ERROR: " + res.status + " Error Count:" + (++errorCount));
 
-      if (limit > 0 && deletedMessagesCount >= limit) {
-        logMessage("Message limit reached, stopping execution.");
-        return;
-      }
-    } else {
-      logMessage("Ignoring message sent by another user.");
+                if (limit > 0 && deletedMessagesCount >= limit)
+                    return logMessage("Message limit reached, stopping execution.");
+
+            } else
+                logMessage("Ignoring message sent by another user.");
+
+        }
+
+        clearMessages();
+    } catch (error) {
+        console.error(error);
+        logMessage(`Error: An error occurred while fetching messages from the Discord API. This may be due to network issues, try again later. Error message: ${error.message}`);
     }
-  }
-
-  firstIteration = false; // Set the firstIteration flag to false
-  clearMessages();
-  } catch (error) {
-    logMessage(`Error: An error occurred while fetching messages from the Discord API. This may be due to network issues, try again later. Error message: ${error.message}`);
-  }
 }
 
 // Start clearing messages in the specified channel
